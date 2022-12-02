b36346c737d9d024781f02e1eb3789acc17cac9b
Mohamad Sirojul Munir <sirojulmunir616@gmail.com>
2022-12-02 20:35:25 +0700

    update textloop

--- src/App.js
+++ src/App.js
@@ -42,7 +42,7 @@ function App() {
                 <span className="highlight">CSS</span>
                 <span className="highlight">Javascript</span>
                 <span className="highlight">Go</span>
-                <span className="highlight">Mysql</span>
+                <span className="highlight">PostgreSQL</span>
                 <span className="highlight">Bootstrap</span>
                 <span className="highlight">TailwindCss</span>
                 <span className="highlight">React JS</span>
