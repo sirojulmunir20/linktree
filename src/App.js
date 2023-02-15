830553d814cf3049652821db96acd900fdabdfeb
Mohamad Sirojul Munir <sirojulmunir616@gmail.com>
2023-02-15 21:24:07 +0700

    change emot

--- src/App.js
+++ src/App.js
@@ -31,7 +31,7 @@ function App() {
             <div className="bg_content rd_12 p_8">
             <h1>@Munir</h1>
               <p className="layanan mt-16">
-                <strong>Learn Tech🚀</strong>
+                <strong>Learn Tech 🚀</strong>
               </p>
               <Typist cursor={config_cursor}>Accounting Student at Universitas Islam Malang | Ex-Backend Engineering Student at Ruangguru.</Typist>
             </div>
